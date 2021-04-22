2c4a73dc74298130d92237eaaaa75aebf7e0bff5
JabinGP <1748511181@qq.com>
2021-04-23 01:08:02 +0800

    tmp 3

--- packages/lib/SyncTargetAliyunOSS.ts
+++ packages/lib/SyncTargetAliyunOSS.ts
@@ -2,7 +2,15 @@ import Setting from './models/Setting';
 import Synchronizer from './Synchronizer';
 import { _ } from './locale.js';
 import BaseSyncTarget from './BaseSyncTarget';
+import { FileApi } from './file-api';
+import FileApiDriverAliyunOSS from './file-api-driver-aliyun-oss';
+import OSS = require('ali-oss');
 // import { FileApi } from './file-api';
+
+const region = 'oss-cn-shenzhen';
+const accKeyId = 'LTAI5t6P4eAsGJ3Cmc2y4USv';
+const accKeySecret = 'QksBjoxhGo16PJfP4GjCskryIsD6LP';
+const bucket = 'jabin-dev-oss';
 
 interface FileApiOptions {
 	path(): string;
@@ -12,6 +20,7 @@ interface FileApiOptions {
 }
 
 export default class SyncTargetAliyunOSS extends BaseSyncTarget {
+	private api_: OSS;
 
 	public static id() {
 		// Find this number
@@ -30,6 +39,22 @@ export default class SyncTargetAliyunOSS extends BaseSyncTarget {
 		return `${_('Aliyun OSS')} (Beta)`;
 	}
 
+	public ossParameters() {
+		return {
+			region: region,
+			accessKeyId: accKeyId,
+			accessKeySecret: accKeySecret,
+			bucket: bucket,
+		};
+	}
+
+	public api(): OSS {
+		if (this.api_) return this.api_;
+
+		this.api_ = new OSS(this.ossParameters());
+		return this.api_;
+	}
+
 	public async isAuthenticated() {
 		return true;
 	}
@@ -38,28 +63,45 @@ export default class SyncTargetAliyunOSS extends BaseSyncTarget {
 	//	return super.fileApi();
 	// }
 
-	// private static async newFileApi_(options: FileApiOptions) {
-	// 	return options;
-	// }
+	private static async newFileApi_(options: FileApiOptions) {
+		return options;
+	}
 
 	public static async checkConfig(options: FileApiOptions) {
+		console.log(options);
 		const output = {
 			ok: false,
 			errorMessage: '',
 		};
 
-		try {
-			console.log(options);
-		} catch (error) {
-			output.errorMessage = error.message;
-			if (error.code) output.errorMessage += ` (Code ${error.code})`;
-		}
+		// try {
+		const params = {
+			region: region,
+			accessKeyId: accKeyId,
+			accessKeySecret: accKeySecret,
+			bucket: bucket,
+		};
 
-		return options;
+		const driver = new FileApiDriverAliyunOSS(new OSS(params));
+		const fileApi = new FileApi('', driver);
+		fileApi.setSyncTargetId(this.id());
+
+		const api = fileApi.driver().api() as OSS;
+		await api.getBucketInfo(params.bucket);
+		output.ok = true;
+		// } catch (error) {
+		// 	output.errorMessage = error.message;
+		// 	if (error.code) output.errorMessage += ` (Code ${error.code})`;
+		// }
+
+		return output;
 	}
 
 	protected async initFileApi() {
-		return 9999;
+		const appDir = '';
+		const fileApi = new FileApi(appDir, new FileApiDriverAliyunOSS(this.api()));
+		fileApi.setSyncTargetId(SyncTargetAliyunOSS.id());
+		return fileApi;
 	}
 
 	protected async initSynchronizer() {
